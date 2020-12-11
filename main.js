385c2b68c49d7fee39c098d3eea0677f60375899
yeebinrong <yeebinrong@gmail.com>
2020-12-11 20:05:47 +0800

    fixed sql insert and select

--- main.js
+++ main.js
@@ -39,10 +39,11 @@ app.use(bodyParser.json())
 // SQL STATEMENTS
 const INSERT_TODO_RETURN_ID = 'INSERT INTO todo (name, due) VALUES (?,?)'
 const INSERT_TASKS = 'INSERT INTO tasks (id, description, priority) VALUES ?'
-const SELECT_LAST_INSERT_ID = 'SELECT LAST_INSERT_ID() as id'
 const SELECT_ALL_TODO = 'SELECT * FROM todo'
 const SELECT_TODO_WITH_ID = 'SELECT * FROM todo WHERE id = ?'
 const SELECT_TASKS_WITH_ID = 'SELECT * FROM tasks WHERE id = ?'
+const DELETE_TODO_WITH_ID = 'DELETE FROM todo WHERE id = ?';
+const DELETE_ALL_TASK_WITH_ID = 'DELETE FROM tasks WHERE id = ?';
 
 // Make SQL Query
 makeQuery = (STMT, POOL) => {
@@ -62,23 +63,43 @@ makeQuery = (STMT, POOL) => {
 // SQL QUERIES
 const QUERY_INSERT_TODO_RETURN_ID = makeQuery(INSERT_TODO_RETURN_ID, POOL)
 const QUERY_INSERT_TASKS = makeQuery(INSERT_TASKS, POOL)
-const QUERY_SELECT_LAST_INSERT_ID = makeQuery(SELECT_LAST_INSERT_ID, POOL)
 const QUERY_SELECT_ALL_TODO = makeQuery(SELECT_ALL_TODO, POOL)
 const QUERY_SELECT_TODO_WITH_ID = makeQuery(SELECT_TODO_WITH_ID, POOL) 
 const QUERY_SELECT_TASKS_WITH_ID = makeQuery(SELECT_TASKS_WITH_ID, POOL)
+const QUERY_DELETE_TODO_WITH_ID = makeQuery(DELETE_TODO_WITH_ID, POOL)
+const QUERY_DELETE_ALL_TASKS_WITH_ID = makeQuery(DELETE_ALL_TASK_WITH_ID, POOL)
 
 // #### RESOURCES #### 
+
+// ### DELETE REQUESTS ###
+// ## DELETE /API/TODO/ID ##
+app.delete('/api/todo/:id', async (req, resp) => {
+    const id = req.params.id
+    console.info("ID IS : ", id)
+    try {
+        await QUERY_DELETE_TODO_WITH_ID(id)
+        await QUERY_DELETE_ALL_TASKS_WITH_ID(id)
+        resp.status(200)
+        resp.type('application/json')
+        resp.json({})
+    } catch (e) {
+        console.error("Error deleting: ", e)
+        resp.status(404)
+        resp.type('application/json')
+        resp.json({})
+    }
+})
 
 // ### POST REQUESTS ###
 // ## POST /API/UPLOAD ##
 app.post('/api/upload', upload.none('data'), async (req, resp) => {
     const data = JSON.parse(req.body.data)
-    await QUERY_INSERT_TODO_RETURN_ID([data['name'], data['due']])
-    const id = (await QUERY_SELECT_LAST_INSERT_ID())[0].id;
+    console.info("POSTING", data)
+    const id = (await QUERY_INSERT_TODO_RETURN_ID([data['name'], data['due']])).insertId
     const taskArray = data.tasks.map(task => {
         return [id, task.description, task.priority]
     })
-    if (data.tasks.length > 0) {
+    if (taskArray.length > 0) {
         await QUERY_INSERT_TASKS([taskArray])
     }
     resp.status(200)
@@ -87,10 +108,10 @@ app.post('/api/upload', upload.none('data'), async (req, resp) => {
 })
 
 // ### GET REQUESTS ###
-// ## GET /API/TODO ##
+// ## GET /API/TODO/?ID ##
 app.get('/api/todo/:id', async (req, resp) => {
     const id = req.params.id
-    if (id == 'all') {
+    if (id === 'all') {
         const result = await QUERY_SELECT_ALL_TODO()
         resp.status(200)
         resp.type('application/json')
@@ -106,9 +127,16 @@ app.get('/api/todo/:id', async (req, resp) => {
             }
         })
         results.tasks = tasks;
-        resp.status(200)
-        resp.type('application/json')
-        resp.json(results)
+        console.info(results)
+        if (!results.name) {
+            resp.status(404)
+            resp.type('application/json')
+            resp.json({error:"Not found."})
+        } else {
+            resp.status(200)
+            resp.type('application/json')
+            resp.json(results)
+        }
     }
 })
 
